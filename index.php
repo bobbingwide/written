e8ec17f6825682aca81ffc15d24f5c7726fc6cf1
Herb Miller <herb@bobbingwide.com>
2021-07-02 15:18:07 +0100

    index.php is needed for parent themes

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Apparently this can be empty.
+_e( 'Activate Gutenberg to use the "Written" theme', 'written');
+?>
+    <br />
+<?php
+_e( 'You also need an index.html file in block-templates', 'written');
